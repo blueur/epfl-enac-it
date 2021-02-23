aacb248a8dbcc7959b3fe6e23a2d273c7491950b
blueur <blueur-ii@hotmail.com>
2021-02-23 11:49:36 +0100

    geotransform

--- data-acquisition.py
+++ data-acquisition.py
@@ -1,60 +1,90 @@
+import logging
+import time
+from typing import Tuple
+
 import ee
 # import shapefile
 import geemap
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import rasterio
+from affine import Affine
+from rasterio.plot import show
+
+CHIRPS_DAILY = 'UCSB-CHG/CHIRPS/DAILY'
+INT_NAN = -1
+
+logger = logging.getLogger(__name__)
 
 
 def greater_than_zero(image: ee.Image):
     return image.gt(ee.Image.constant(0))
 
 
-def get_precipitations(scale=1000) -> pd.DataFrame:
+def get_precipitations(scale=1000, start='2020-01-01', end='2021-01-01') -> pd.DataFrame:
     ee.Initialize()
-    area = geemap.shp_to_ee("data/aoi.shp")
-    image = (ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
-             # .filter(ee.Filter.geometry(area))
-             .filter(ee.Filter.date('2020-01-01', '2020-02-01'))
+    area = geemap.shp_to_ee('data/aoi.shp')
+    image = (ee.ImageCollection(CHIRPS_DAILY)
+             .filter(ee.Filter.date(start, end))
              .map(greater_than_zero)
              .sum())
     image_collection = ee.ImageCollection(image)
     # https://developers.google.com/earth-engine/apidocs/ee-imagecollection-getregion
     region = image_collection.getRegion(geometry=area, scale=scale)
+    logger.info(f'getting image from {start} to {end}')
+    start_time = time.time()
     data = region.getInfo()
     data_frame = pd.DataFrame.from_records(data[1:], columns=data[0])
-    print(data_frame)
+    logger.info(f'received {data_frame.size:,} coordinates in {time.time() - start_time:0.3f} seconds')
     return data_frame
 
 
-def export_raster(array: np.ndarray) -> None:
-    profile = rasterio.profiles.DefaultGTiffProfile(count=1)
-    print(profile)
-    array = array * 8  # FIXME
-    with rasterio.open('output.tif', 'w', height=array.shape[0], width=array.shape[1], **profile) as dst:
-        dst.write(array.astype(np.uint8), 1)
+def export_intarray_to_raster(array: np.ndarray, affine: Affine, filename: str) -> None:
+    profile = rasterio.profiles.DefaultGTiffProfile(count=1, dtype=array.dtype)
+    height = array.shape[0]
+    width = array.shape[1]
+    logger.info(f'saving {filename} ({width} x {height}) with profile {profile}')
+    with rasterio.open(filename, 'w', height=height, width=width, transform=affine, **profile) as dst:
+        dst.write(array, 1)
 
 
-def show_raster(array: np.ndarray) -> None:
-    plt.ion()
-    plt.imshow(array)
-    plt.ioff()
-    plt.show()
+def export_array_to_raster(array: np.ndarray, affine: Affine, filename: str) -> None:
+    array[np.isnan(array)] = INT_NAN
+    export_intarray_to_raster(array.astype(np.int8), affine, filename)
 
 
-def get_grid(data: pd.DataFrame) -> np.ndarray:
-    # https://www.hatarilabs.com/ih-en/how-to-create-a-geospatial-raster-from-xy-data-with-python-pandas-and-rasterio-tutorial
-    array = (data
-             .pivot(index='latitude', columns='longitude', values='precipitation')
-             .sort_index(ascending=False))
-    print(array)
-    return array.to_numpy()
+def show_raster(filename: str) -> None:
+    with rasterio.open(filename) as src:
+        show(src)
 
 
-# arr = np.random.randint(30, size=(100,100)).astype(np.uint8)
+def get_affine(longitudes: pd.Float64Index, latitudes: pd.Float64Index) -> Affine:
+    # https://github.com/sgillies/affine#usage-with-gis-data-packages
+    longitude_resolution = (longitudes[-1] - longitudes[0]) / longitudes.size
+    latitude_resolution = (latitudes[-1] - latitudes[0]) / latitudes.size
+    return Affine.from_gdal(longitudes[0], longitude_resolution, 0.0,
+                            latitudes[0], 0.0, latitude_resolution)
 
-precipitations = get_precipitations()
-grid = get_grid(precipitations)
-export_raster(grid)
-show_raster(grid)
+
+def get_array(data: pd.DataFrame) -> Tuple[np.ndarray, Affine]:
+    data_frame = (data
+                  .pivot(columns='longitude', index='latitude', values='precipitation')
+                  .sort_index(ascending=False))
+    logger.debug(data_frame)
+    affine = get_affine(data_frame.columns, data_frame.index)
+    array = data_frame.to_numpy()
+    return array, affine
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s:\n%(message)s', level=logging.INFO)
+    # logger.setLevel(logging.DEBUG)
+    filename = 'output.tif'
+    precipitations = get_precipitations()
+    array, affine = get_array(precipitations)
+    export_array_to_raster(array, affine, filename)
+    show_raster(filename)
+
+
+if __name__ == "__main__":
+    main()
